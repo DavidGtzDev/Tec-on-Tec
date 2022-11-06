278d78861637f6182da3e9ba43899da3151c4b36
David Emiliano Gutiérrez Leal <davidgtzleal@gmail.com>
2022-11-05 21:43:07 -0600

    Filter finally works :)

--- src/scripts/filter.js
+++ src/scripts/filter.js
@@ -1,5 +1,6 @@
 export default function filter(arr,date){
     let newArr = []
+    let temp = []
     const months = [
         {
             "month": "Jan",
@@ -60,14 +61,6 @@ export default function filter(arr,date){
             }
         }
     }
-    
-    //Quedarte solo con las materias en las que el año sea igual
-    //HAY UN BUG AQUÍ
-    for(let i = 0; i< newArr.length; i++){
-        if(date.toString().split(" ")[3] != newArr[i]["START_DATE"].split(" ")[2]){
-            newArr.splice(i,1)
-        }
-    }
     //Quedarte solo con las materias que estén en el rango de meses (asignar cada mes a un numero y ver que esté entre el rango)
 
     
@@ -75,12 +68,14 @@ export default function filter(arr,date){
     for(let j = 0; j< newArr.length; j++){
         let monthStart = months.find(o => o.month === newArr[j]["START_DATE"].split(" ")[0])["number"]
         let monthEnd = months.find(o => o.month === newArr[j]["END_DATE"].split(" ")[0])["number"]
-
-        
-        if(monthNum < monthStart || monthNum > monthEnd){
-            newArr.splice(j,1)
+       
+        if(monthNum >= monthStart && monthNum <= monthEnd ){
+            temp.push(newArr[j])
         }
     }
+    newArr = temp
+    temp = []
+    
     
     //Quedarte solo con las materias que estén en el rango de días (dentro del mes seleccionado, ver si la materia termina ese mes)
     //SE PASA POR UNA SEMANA
@@ -91,15 +86,20 @@ export default function filter(arr,date){
         let endDay = newArr[k]["END_DATE"].split(" ")[1]
 
         if(monthNum == monthStart){
-            if(parseInt(date.toString().split(" ")[2]) < startDay){
-                newArr.splice(k,1)
+            if(parseInt(date.toString().split(" ")[2]) >= startDay){
+                temp.push(newArr[k])
             }
         }else if(monthNum == monthEnd){
-            if(parseInt(date.toString().split(" ")[2]) > endDay){
-                newArr.splice(k,1)
+            if(parseInt(date.toString().split(" ")[2]) <= endDay){
+                temp.push(newArr[k])
             }
+        }else{
+            temp.push(newArr[k])
         }
 
     }
+    newArr = temp
+    temp = []
+
     return newArr
 }
