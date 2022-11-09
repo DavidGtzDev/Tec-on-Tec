617d8878539c64099da71343d3a2db944a0fcda2
luis-ddxy <luis.garza.sanchez10@gmail.com>
2022-11-09 07:02:43 -0600

    Header implemented

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import "../styles/topbar.css"
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 export default function Topbar(props) {
   
   return (
-    <div class="container-fluid bg-primary">
-        <div>
-          <img src={props.url} className='profile_picture' onClick={e => signInWithPopup(props.auth, new GoogleAuthProvider())}></img>
-        </div>
-        <div></div>
+    <div class="container-fluid bg-dark text-center">
+      <button type="button" class="btn btn-dark rounded float-end" onClick={e => signOut(props.auth)}>
+        <i class="bi bi-box-arrow-right fs-4"></i>
+      </button>
+
+      <button type="button" class="btn btn-dark rounded float-end">
+        <img src={props.url} alt={"P"} height="35rem" class="rounded-circle profile_picture" onClick={e => signInWithPopup(props.auth, new GoogleAuthProvider())}></img>
+      </button>
+      <div></div>
     </div>
 
     // <Box sx={{ flexGrow: 1 }} style={{width: "100vw"}}>
