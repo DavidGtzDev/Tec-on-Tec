b317ab7ab71625893bf34992a00af6e4662a23cb
David Emiliano Gutiérrez Leal <davidgtzleal@gmail.com>
2022-11-09 18:08:42 -0600

    Reimplemented cards

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -12,18 +12,21 @@ export default function CardUser(props) {
    let images = ["https:payload.cargocollective.com/1/6/222271/10585229/pattern-c_800.jpg","https:i.pinimg.com/originals/b0/27/db/b027dbf5efb3e939bcd03e46b5d39afc.jpg","https:i.etsystatic.com/13787576/r/il/92f95d/3261081855/il_fullxfull.3261081855_i5ht.jpg","https:media.graphassets.com/JBvG8dAPR7yNGL1dB4QX","https:media.graphassets.com/V9q4iS88TaiiBDDkmPkE","https:d30g5rxy3ee0r1.cloudfront.net/wp-content/uploads/2018/03/8708207_by_LAYLA95353_9e5515ebbecc7c7b7df1078a42b7c9fb-nautical-design-pretty-patterns-450x450.jpg","https:susanbranch.com/wp-content/uploads/2018/04/Print2-500x334.jpg"]
 
   return (
-    //<div class="card mb-3">
-      //<div class="card-body">
-        //<h5 class="card-title">{props.obj["NAME"]}</h5>
-        //<h6 class="card-subtitle text-muted">{props.obj["CODE"]}</h6>
-        //{props.obj["START_TIME"]} - {props.obj["END_TIME"]}
-        //<br></br>
-        //Aula {props.obj["CLASSROOM"]}
-        //<br></br>
-        //Profesor {props.obj["PROFESSOR_LIST"][0]}
-      //</div>
-    //</div>
+    /*
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">{props.obj["NAME"]}</h5>
+        <h6 class="card-subtitle text-muted">{props.obj["CODE"]}</h6>
+        {props.obj["START_TIME"]} - {props.obj["END_TIME"]}
+        <br></br>
+        Aula {props.obj["CLASSROOM"]}
+        <br></br>
+        Profesor {props.obj["PROFESSOR_LIST"][0]}
+      </div>
+    </div>
+    */
      <Card sx={{ maxWidth: 700 }} style={{marginBottom: 20}}>
+      {/*
        <CardMedia
          component="img"
          alt="green iguana"
@@ -31,6 +34,7 @@ export default function CardUser(props) {
          image= {images[Math.floor(Math.random()*images.length)]}
        >
        </CardMedia>
+       */}
        <CardContent>
          <Typography gutterBottom variant="h5" component="div">
            {props.obj["NAME"]}
@@ -56,5 +60,6 @@ export default function CardUser(props) {
          </AccordionDetails>
        </Accordion>
      </Card>
+    
   )
 }
