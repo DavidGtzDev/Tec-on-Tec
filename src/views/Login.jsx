5b6271c80a8898328dcfdb0a5043f47eef3d611c
marcarro <A00837236@tec.mx>
2022-11-20 16:12:43 -0600

    Hacer responsivo el boton sign in

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -7,7 +7,7 @@ export default function Login(props) {
   return (
     <div style={{display:"flex",flexDirection:"column", justifyContent:"center", alignItems:"center", height: "100vh", backgroundColor: "#212529"}}>
         <img src="/TecOnTime.png" style={{height:150,marginBottom:20}}></img>
-        <Fab variant="extended" onClick={e => signInWithPopup(props.auth, new GoogleAuthProvider())}style={{width:"30vw"}} >
+        <Fab variant="extended" onClick={e => signInWithPopup(props.auth, new GoogleAuthProvider())} >
         <img src='/2000px-Google_G_Logo.svg_.png' style={{height:30, marginRight:20}}></img>
           Consulta tu horario
         </Fab>
