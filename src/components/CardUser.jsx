0538df3759a7a355d68b64738dbbdf85040036e6
luis-ddxy <luis.garza.sanchez10@gmail.com>
2022-11-21 13:59:40 -0600

    Revert "New data"
    This reverts commit 3e2f12a679c4ad0da98c7ca5ebbad84329da205c.

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -9,9 +9,6 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
  import "../styles/card.css"
 
 export default function CardUser(props) {
-  const str = props.obj["NAME"] + " || " + props.obj["CODE"] + " || " + props.obj["CRN"]
-  const length = 50;
-  const trimmed = str.substring(0, length) + "...";
   return (
     /*
     <div class="card mb-3">
@@ -38,13 +35,16 @@ export default function CardUser(props) {
        */}
        <CardContent>
          <Typography gutterBottom variant="h5" component="div">
-            {trimmed}
+           {props.obj["NAME"]}
          </Typography>
          <Typography variant="body2" color="text.secondary">
            {props.obj["START_TIME"]} - {props.obj["END_TIME"]}
          </Typography>
          <Typography variant="body2" color="text.secondary">
           Salón: {props.obj["CLASSROOM"]}
+         </Typography>
+         <Typography variant="body2" color="text.secondary">
+         Clave: {props.obj["CODE"]} <br></br>
          </Typography>
          <Typography variant="body2" color="text.secondary">
          Profesor: {props.obj["PROFESSOR_LIST"][0]} <br></br>
